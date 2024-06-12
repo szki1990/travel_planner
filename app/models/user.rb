a3de07a220d1956d41f5dfb3e832f77f8aedcc72
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-06-12 07:39:19 +0000

    [CRUD]C

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_one_attached :profile_image
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/25145460.jpg')
+      profile_image.attach(io: File.open(file_path), filename: '25145460.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end 
+         
   validates :email, :name, presence: { message: "が入力されていません" }
 end
