6c908be4e082b0a37f9ad4c1d6cb6221c1124576
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-06-17 14:09:49 +0000

    [layout]

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :users
   
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
   get 'homes/top', to: 'homes#top' 
   get 'mypage', to:'users#my_page', as:'my_page'
   root 'homes#top'
   get 'about', to: 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update, :destroy]
-  resources :posts, only: [:index]
+  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  #resources :posts, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
