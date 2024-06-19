e7d5d3114266e51f7e277a3ed545e7a20b61fac4
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-06-19 02:49:23 +0000

    [bootstrap top]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     about_path
   end 
-  
+
   protected
   
   def configure_permitted_parameters
