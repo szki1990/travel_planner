ec480644c26d13df7f0fe3e65dd05233ed1465ea
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-07-08 03:26:20 +0000

    [cost_new_layout]

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -2,6 +2,7 @@ class Cost < ApplicationRecord
   belongs_to :book
   validates :price, presence: true
   validates :date, presence: true
+  validates :purchase_item, presence: true
   validates :payment_method, presence: true
   PAYMENT_METHODS = ['クレジットカード', '現金', '電子マネー', 'ポイント', 'その他'].freeze
 end
