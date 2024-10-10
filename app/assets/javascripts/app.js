7cc9fc69af491ff64b1bce67797b66ca949b42cd
szki1990 <163372203+szki1990@users.noreply.github.com>
2024-10-10 02:56:50 +0000

    java test dark

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,11 @@
-alert('Hello world');
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#dark-mode-toggle").addEventListener("click", () => {
+        if (document.body.classList.contains("dark-mode")) {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "disabled");
+        } else {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "enabled");
+        }
+    });
+});
