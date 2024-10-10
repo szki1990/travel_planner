c11677b738c36049f7f4ee77a079e46f7091bb2b
szki1990 <163372203+szki1990@users.noreply.github.com>
2024-10-10 04:52:04 +0000

    about　Fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,30 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("turbolinks:load", () => {
   const heading = document.querySelector('#heading');
-  
+
   if (heading) {
     const keyframes = {
       opacity: [0, 1],
       translate: ['0 50px', 0],
     };
-    
+
     const options = {
       duration: 2000,
       easing: 'ease',
     };
-    
+
     heading.animate(keyframes, options);
   }
-  const items = document.querySelectorAll('.img-item');
+
+  if (window.location.pathname === '/about') {
+    const items = document.querySelectorAll('.img-item');
 
     for (let i = 0; i < items.length; i++) {
-        const keyframes = {
-            opacity: [0, 1]
-        };
-        const options = {
-            duration: 600,
-            delay: i * 300,
-            fill: 'forwards',
-        };
-        items[i].animate(keyframes, options);
+      const keyframes = {
+        opacity: [0, 1],
+      };
+      const options = {
+        duration: 600,
+        delay: i * 300,
+        fill: 'forwards',
+      };
+      items[i].animate(keyframes, options);
     }
+  }
 });
