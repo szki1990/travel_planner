521a63286fc8607180227b0fd739b158a61aad21
szki1990 <163372203+szki1990@users.noreply.github.com>
2024-10-10 04:34:32 +0000

    about

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#dark-mode-toggle").addEventListener("click", () => {
-        if (document.body.classList.contains("dark-mode")) {
-            document.body.classList.remove("dark-mode");
-            localStorage.setItem("darkMode", "disabled");
-        } else {
-            document.body.classList.add("dark-mode");
-            localStorage.setItem("darkMode", "enabled");
-        }
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const heading = document.querySelector('#heading');
+  
+  if (heading) {
+    const keyframes = {
+      opacity: [0, 1],
+      translate: ['0 50px', 0],
+    };
+    
+    const options = {
+      duration: 2000,
+      easing: 'ease',
+    };
+    
+    heading.animate(keyframes, options);
+  }
+  const items = document.querySelectorAll('.img-item');
+
+    for (let i = 0; i < items.length; i++) {
+        const keyframes = {
+            opacity: [0, 1]
+        };
+        const options = {
+            duration: 600,
+            delay: i * 300,
+            fill: 'forwards',
+        };
+        items[i].animate(keyframes, options);
+    }
 });
